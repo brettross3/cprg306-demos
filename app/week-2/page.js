dce9b38fcb474bce7c909ee999c30a9ee417b395
brettross3 <brett.ross@edu.sait.ca>
2024-01-16 12:58:10 -0700

    cprg306demo

---
+++ app/week-2/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <main>
+            <h1 className="text-4xl">Week 2 Demos</h1>
+            <p>This is a paragraph.</p>
+        </main>
+    );
+}
