041cfa7df36f665da4c1461be862463cd004ed61
brettross3 <brett.ross@edu.sait.ca>
2024-01-16 12:58:46 -0700

    Update page.js

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -3,6 +3,7 @@ export default function Page() {
         <main>
             <h1 className="text-4xl">Week 2 Demos</h1>
             <p>This is a paragraph.</p>
+            <p>This is another paragraph.</p>
         </main>
     );
 }
